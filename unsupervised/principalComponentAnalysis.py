ea3c56ca1d27076ba4bbad7ab4e794a00101030f
SaadChaouki <61946524+SaadChaouki@users.noreply.github.com>
2021-01-31 18:25:51 +0000

    Completed principal component analysis.

--- unsupervised/principalComponentAnalysis.py
+++ unsupervised/principalComponentAnalysis.py
@@ -2,7 +2,7 @@
 from utils.devutils import generateClassificationData
 from utils.functions import covarianceMatrix
 import numpy as np
-
+import matplotlib.pyplot as plt
 
 class PCA():
     def __init__(self, nComponents):
@@ -11,11 +11,14 @@ class PCA():
     def transform(self, x):
         covMatrix = covarianceMatrix(x)
         eigenValues, eigenVectors = np.linalg.eigh(covMatrix)
-        print(eigenValues)
+        sortedIdx = np.argsort(eigenValues)[::-1]
+        eigenVectors = eigenVectors[:, sortedIdx][:, :self.nComponents]
+        return x.dot(eigenVectors)
 
 if __name__ == '__main__':
     pca = PCA(2)
-    x, y = generateClassificationData(100)
-    x[:, 0] = x[:, 1]
-    pca.transform(x)
+    x, y = generateClassificationData(1000)
+    t = pca.transform(x)
+
+
 
