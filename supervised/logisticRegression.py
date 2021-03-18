6047b336e3e367db68adc23f05f9d8a0d5ffd315
Saad Chaouki <sd.chaouki@gmail.com>
2021-03-18 09:57:54 +0000

    Re-organised Logistic Regression code

--- supervised/logisticRegression.py
+++ supervised/logisticRegression.py
@@ -1,26 +1,25 @@
 import math
-
 import numpy as np
-
 from utils.activationFunctions import sigmoid
 
 
-class LogisticRegression():
+class LogisticRegression(object):
 
-    def __init__(self, learningRate=.1):
-        self.learningRate = learningRate
+    def __init__(self, learning_rate=.1, iterations=5000):
+        self.learning_rate = learning_rate
+        self.iterations = iterations
 
-    def fit(self, x, y, iterations=5000):
+    def fit(self, x, y):
         # Initialize the weights
         limit = 1 / math.sqrt(x.shape[1])
         self.weights = np.random.uniform(-limit, limit, (x.shape[1],))
 
-        for i in range(iterations):
-            yPredicted = self.predictProbabilities(x)
-            self.weights -= self.learningRate * (np.dot(x.T, yPredicted - y) / x.shape[0])
+        for i in range(self.iterations):
+            yPredicted = self.predict_probabilities(x)
+            self.weights -= self.learning_rate * (np.dot(x.T, yPredicted - y) / x.shape[0])
 
-    def predictProbabilities(self, x):
+    def predict_probabilities(self, x):
         return sigmoid(np.dot(x, self.weights))
 
-    def predictClasses(self, x):
-        return np.round(self.predictProbabilities(x), 0)
+    def predict_classes(self, x):
+        return np.round(self.predict_probabilities(x), 0)
