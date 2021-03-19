5198c404136252427841ea3ecb1c02bf39ae2418
Saad Chaouki <sd.chaouki@gmail.com>
2021-03-19 10:01:09 +0000

    Updated naming convention PCA

--- unsupervised/principalComponentAnalysis.py
+++ unsupervised/principalComponentAnalysis.py
@@ -1,21 +1,14 @@
-from utils.devutils import generateClassificationData
-from utils.functions import covarianceMatrix
+from utils.functions import covariance_matrix
 import numpy as np
 
 
 class PCA():
-    def __init__(self, nComponents):
-        self.nComponents = nComponents
+    def __init__(self, num_components):
+        self.num_components = num_components
 
-    def transform(self, x):
-        covMatrix = covarianceMatrix(x)
-        eigenValues, eigenVectors = np.linalg.eigh(covMatrix)
-        sortedIdx = np.argsort(eigenValues)[::-1]
-        eigenVectors = eigenVectors[:, sortedIdx][:, :self.nComponents]
-        return x.dot(eigenVectors)
-
-
-if __name__ == '__main__':
-    pca = PCA(2)
-    x, y = generateClassificationData(1000)
-    t = pca.transform(x)
+    def transform(self, X):
+        covariance = covariance_matrix(X)
+        eigen_values, eigen_vectors = np.linalg.eigh(covariance)
+        sorted_idx = np.argsort(eigen_values)[::-1]
+        eigen_vectors = eigen_vectors[:, sorted_idx][:, :self.num_components]
+        return X.dot(eigen_vectors)
