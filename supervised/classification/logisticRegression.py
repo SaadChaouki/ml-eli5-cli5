8614e8d351c7c6aee58a4cfe0d98645243374440
Saad Chaouki <sd.chaouki@gmail.com>
2021-03-19 14:59:54 +0000

    Updated logistic regression to use base model regression

--- supervised/classification/logisticRegression.py
+++ supervised/classification/logisticRegression.py
@@ -1,25 +1,16 @@
-import math
 import numpy as np
 from utils.activationFunctions import sigmoid
+from supervised.base.baseRegression import BaseRegression
 
 
-class LogisticRegression(object):
+class LogisticRegression(BaseRegression):
 
-    def __init__(self, learning_rate=.1, iterations=5000):
-        self.learning_rate = learning_rate
-        self.iterations = iterations
+    def __init__(self, learning_rate=.01, iterations=5000):
+        self.regularization = lambda x: 0
+        super(LogisticRegression, self).__init__(iterations=iterations, learning_rate=learning_rate)
 
-    def fit(self, x, y):
-        # Initialize the weights
-        limit = 1 / math.sqrt(x.shape[1])
-        self.weights = np.random.uniform(-limit, limit, (x.shape[1],))
+    def predict(self, X):
+        return sigmoid(np.dot(X, self.weights))
 
-        for i in range(self.iterations):
-            yPredicted = self.predict_probabilities(x)
-            self.weights -= self.learning_rate * (np.dot(x.T, yPredicted - y) / x.shape[0])
-
-    def predict_probabilities(self, x):
-        return sigmoid(np.dot(x, self.weights))
-
-    def predict_classes(self, x):
-        return np.round(self.predict_probabilities(x), 0)
+    def predict_classes(self, X):
+        return np.round(self.predict(X), 0)
