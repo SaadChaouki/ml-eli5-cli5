2940258315eb330252cd85ec96f7383da2dfd866
Saad Chaouki <sd.chaouki@gmail.com>
2021-03-20 18:59:40 +0000

    Moved measure distances to another file

--- unsupervised/localOutlierFactor.py
+++ unsupervised/localOutlierFactor.py
@@ -1,12 +1,5 @@
 import numpy as np
-
-
-def manhattan_distance(point, X):
-    return np.sum(np.abs(point - X), axis=1, dtype=np.float)
-
-
-def euclidean_distance(point, X):
-    return np.sqrt(np.sum(np.square(point - X), axis=1))
+from utils.distance_measures import manhattan_distance, euclidean_distance
 
 
 class LocalOutlierFactor(object):
