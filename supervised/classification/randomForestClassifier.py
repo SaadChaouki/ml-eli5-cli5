fa0764b542b2db52a719c5db211d69b0230e2076
Saad Chaouki <sd.chaouki@gmail.com>
2021-03-21 21:46:37 +0000

    Updated name of parameter

--- supervised/classification/randomForestClassifier.py
+++ supervised/classification/randomForestClassifier.py
@@ -4,9 +4,9 @@ import numpy as np
 
 
 class RandomForestClassifier(BaseRandomForest):
-    def __init__(self, num_estimators=100, max_depth=2, max_features=None, min_sample_leaf=10):
+    def __init__(self, num_estimators=100, max_depth=2, max_features=None, minimum_sample_leaf=10):
         super(RandomForestClassifier, self).__init__(num_estimators=num_estimators, max_depth=max_depth,
-                                                     max_features=max_features, min_sample_leaf=min_sample_leaf)
+                                                     max_features=max_features, min_sample_leaf=minimum_sample_leaf)
         self.prediction_combination = self.__majority_vote
         self.tree_model = DecisionTreeClassifier
 
