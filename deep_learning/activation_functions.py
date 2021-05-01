43bd9f3caba848fb11fb2c572f8daeb7550e1b36
Saad Chaouki <sd.chaouki@gmail.com>
2021-05-01 22:50:44 +0100

    Created activation functions

---
+++ deep_learning/activation_functions.py
@@ -0,0 +1,79 @@
+import numpy as np
+
+
+class Sigmoid(object):
+    def __call__(self, x):
+        return 1 / (1 + np.exp(-x))
+
+    def gradient(self, x):
+        return self.__call__(x) * (1 - self.__call__(x))
+
+
+class ReLU(object):
+    def __call__(self, x):
+        return np.where(x >= 0, x, 0)
+
+    def gradient(self, x):
+        return np.where(x >= 0, 1, 0)
+
+
+class TanH(object):
+    def __call__(self, x):
+        return (2 / (1 + np.exp(-2 * x))) - 1
+
+    def gradient(self, x):
+        return 1 - self.__call__(x) ** 2
+
+
+class LeakyReLU(object):
+    def __init__(self):
+        self.alpha = .01
+
+    def __call__(self, x):
+        return np.where(x > 0, x, self.alpha * x)
+
+    def gradient(self, x):
+        return np.where(x > 0, 1, self.alpha)
+
+
+class ELU(object):
+    """ Exponential linear unit """
+
+    def __init__(self, alpha=.1):
+        self.alpha = alpha
+
+    def __call__(self, x):
+        return np.where(x > 0, x, self.alpha * (np.exp(x) - 1))
+
+    def gradient(self, x):
+        return np.where(x > 0, 1, self.__call__(x) + self.alpha)
+
+
+class SELU(object):
+    def __init__(self):
+        self.alpha = 1.6732632423543772848170429916717
+        self.l = 1.0507009873554804934193349852946
+
+    def __call__(self, x):
+        return self.l * np.where(x > 0, x, self.alpha * np.exp(x) - self.alpha)
+
+    def gradient(self, x):
+        return self.l * np.where(x > 0, 1, self.alpha * np.exp(x))
+
+class SoftMax(object):
+    pass
+
+
+if __name__ == '__main__':
+    import matplotlib
+    import matplotlib.pyplot as plt
+
+    matplotlib.use("TkAgg")
+
+    function = SELU()
+
+    x = np.arange(-5, 5, 0.1)
+    y = function(x)
+
+    plt.plot(x, y)
+    plt.show()
