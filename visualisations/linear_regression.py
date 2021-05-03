fc5c4d34858b93f0031b0f2bee404c2093f1fa14
Saad Chaouki <sd.chaouki@gmail.com>
2021-05-03 14:42:16 +0100

    Code cleanup

--- visualisations/linear_regression.py
+++ visualisations/linear_regression.py
@@ -1,11 +1,14 @@
 from supervised.regression.linearRegression import LinearRegression
-from sklearn.datasets import make_regression
-from sklearn.model_selection import train_test_split
-import matplotlib.pyplot as plt
-from utils.metrics import meanSquaredError as mse
-from matplotlib.animation import FuncAnimation
-import matplotlib
 from visualisations.color_palette import two_colors
+from deep_learning.loss import MSELoss
+
+from sklearn.model_selection import train_test_split
+from sklearn.datasets import make_regression
+
+from matplotlib.animation import FuncAnimation
+import matplotlib.pyplot as plt
+import matplotlib
+
 import numpy as np
 import argparse
 
@@ -14,11 +17,12 @@ matplotlib.use("TkAgg")
 
 def update(i):
     y_pred = np.array([x for _, x in sorted(zip(X_train, model.error[i]))])
-    plt.title(f'Iteration: {i + 1} | MSE: {round(mse(y_train, model.error[i]), 2)}')
+    plt.title(f'Iteration: {i + 1} | MSE: {round(MSELoss()(y_train, model.error[i]), 2)}')
     line.set_ydata(y_pred)
 
 
 if __name__ == '__main__':
+
     # Argument parsing.
     parser = argparse.ArgumentParser(description='Visualise a customer Linear Regression model in training.')
     parser.add_argument('--max_iter', type=int, help='Maximum number of iterations.', default=100)
@@ -27,13 +31,15 @@ if __name__ == '__main__':
     parser.add_argument('--test_size', type=float, help='Test set size.', default=.2)
     parser.add_argument('--lr', type=float, help='Learning Rate.', default=.1)
     args = parser.parse_args()
+
+    # Maximum iterations.
     max_iterations = args.max_iter
 
-    # Generate regression data
+    # Generate regression data.
     X, y = make_regression(n_features=1, n_samples=args.n_samples, n_informative=1, noise=30,
                            random_state=args.random_state, bias=500, tail_strength=1)
 
-    # Train - Test Split
+    # Train - Test Split.
     X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=args.test_size, random_state=42)
 
     # Model and Predictions
@@ -44,18 +50,22 @@ if __name__ == '__main__':
     fig, ax = plt.subplots(figsize=(15, 6), dpi=80)
     fig.suptitle('Linear Regression', fontsize=20)
 
-    # Original Data
+    # Plotting training and testing data.
     ax.scatter(X_train, y_train, color=two_colors[0], label='Train Data')
     ax.scatter(X_test, y_test, color=two_colors[1], label='Test Data')
-    plt.xlabel('Feature')
-    plt.ylabel('Target')
 
+    # Plot first iteration line.
     y_pred = np.array([x for _, x in sorted(zip(X_train, model.error[0]))])
     X_train_sorted = np.array(sorted(X_train))
-
     line, = ax.plot(X_train_sorted, y_pred, color='black', linewidth=2, label="Prediction")
 
+    # Labels and legend
+    plt.xlabel('Feature')
+    plt.ylabel('Target')
     plt.legend(loc='lower right')
 
+    # Animation
     animation = FuncAnimation(fig, update, frames=max_iterations, interval=1, repeat=False)
+
+    # Show plot
     plt.show()
