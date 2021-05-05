1947016e065ccf2b00a0e53d3b1af75b5b7f5b3d
Saad Chaouki <sd.chaouki@gmail.com>
2021-05-05 08:01:21 +0100

    Added arguments and code cleanup

--- visualisations/decision_tree.py
+++ visualisations/decision_tree.py
@@ -1,28 +1,44 @@
 from supervised.classification.decisionTreeClassifier import DecisionTreeClassifier
+from visualisations.color_palette import two_colors, two_colors_map
+from utils.metrics import accuracy
+
 from sklearn.datasets import make_classification
+from sklearn.model_selection import train_test_split
+
+from matplotlib.animation import FuncAnimation, PillowWriter
 import matplotlib.pyplot as plt
 import matplotlib
-from sklearn.model_selection import train_test_split
-from matplotlib.animation import FuncAnimation, PillowWriter
-from utils.metrics import accuracy
+
 import numpy as np
-from visualisations.color_palette import two_colors, two_colors_map
+import argparse
+
 
 matplotlib.use("TkAgg")
 
 if __name__ == '__main__':
-    max_depth = 20
 
-    X, y = make_classification(n_samples=1000, n_features=2, n_informative=2, n_redundant=0, random_state=30)
+    # Argument parsing.
+    parser = argparse.ArgumentParser(description='Visualise a custom Decision Tree Classifier model in training.')
+    parser.add_argument('--max_depth', type=int, help='Maximum depth of the tree.', default=15)
+    parser.add_argument('--random_state', type=int, help='Random state for data generation.', default=42)
+    parser.add_argument('--n_samples', type=int, help='Number of data points.', default=1000)
+    parser.add_argument('--test_size', type=float, help='Test set size.', default=.2)
+    args = parser.parse_args()
+
+    # Parameters
+    max_depth = args.max_depth
+
+    X, y = make_classification(n_samples=args.n_samples, n_features=2, n_informative=2, n_redundant=0,
+                               random_state=args.random_state)
 
     # Split train and test data
-    X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=.2, random_state=42)
+    X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=args.test_size, random_state=42)
 
     # Model
     classifiers = [DecisionTreeClassifier(max_depth=i + 1, minimum_sample_leaf=1) for i in range(max_depth)]
 
     # Create decision boundary data
-    h = .05
+    h = 2
     y_min, y_max = X[:, 1].min() - 1, X[:, 1].max() + 1
     x_min, x_max = X[:, 0].min() - 1, X[:, 0].max() + 1
     xx, yy = np.meshgrid(np.arange(x_min, x_max, h), np.arange(y_min, y_max, h))
@@ -58,4 +74,4 @@ if __name__ == '__main__':
     animation = FuncAnimation(fig, update, frames=max_depth, interval=800, repeat=False)
     plt.xlabel('Feature 1')
     plt.ylabel('Feature 2')
-    animation.save('animations/decision_tree.gif', writer=PillowWriter(fps=60))
+    plt.show()
