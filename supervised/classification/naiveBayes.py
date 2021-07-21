15cac4287ca9a0eaccc6668e79a167029b3d6580
Saad Chaouki <sd.chaouki@gmail.com>
2021-07-21 14:22:10 +0100

    Completed naive bayes classifier

--- supervised/classification/naiveBayes.py
+++ supervised/classification/naiveBayes.py
@@ -1,10 +1,49 @@
+import numpy as np
+import math
+
 
 class NaiveBayesClassifier(object):
     def __init__(self):
-        pass
+        self.X = None
+        self.y = None
+        self.classes = None
+        self.params = {}
+        self.priors = {}
 
     def fit(self, X, y):
-        pass
+        # Store the data needed
+        self.X, self.y, self.classes = X, y, np.unique(y)
+
+        # Calculate the priors and the mean/variance for each class
+        for c in self.classes:
+            # Compute the prior for the class
+            self.priors[c] = np.mean(self.y == c)
+            self.params[c] = {}
+
+            # Select data of the class
+            X_c = self.X[self.y == c]
+
+            for i in range(self.X.shape[1]):
+                self.params[c][i] = {'mean': X_c[:, i].mean(), 'var': X_c[:, i].var()}
+
+    def __single_prediction(self, sample):
+        posteriors = {}
+
+        # Compute posteriors for each class
+        for c in self.classes:
+            posterior = self.priors[c]
+            for i, value in enumerate(sample):
+                likelihood = self.__likelihood(self.params[c][i]['mean'], self.params[c][i]['var'], value)
+                posterior *= likelihood
+            posteriors[c] = posterior
+
+        return max(posteriors, key=posteriors.get)
 
     def predict(self, X):
-        pass
+        predictions = np.array([self.__single_prediction(sample) for sample in X])
+        return predictions
+
+    @staticmethod
+    def __likelihood(mean, var, x):
+        eps = 1e-10  # Added in denominator to prevent division by zero
+        return (1.0 / (var * math.sqrt(2.0 * math.pi) + eps)) * math.exp(-.5 * math.pow((x - mean) / (var + eps), 2))
