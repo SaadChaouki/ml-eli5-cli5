d79935ebdfc9c4f394ddb2b855f186ef670c65b6
Saad Chaouki <sd.chaouki@gmail.com>
2021-07-25 12:21:25 +0100

    Started optimizers

---
+++ deep_learning/optimizers.py
@@ -0,0 +1,8 @@
+import numpy as np
+
+class StochasticGradientDescent(object):
+    def __init__(self, learning_rate):
+        self.learning_rate = learning_rate
+
+    def update(self, weights, gradients):
+        pass
