22c6dbad70c74972425eab31653bfd45c77c67cb
Saad Chaouki <sd.chaouki@gmail.com>
2021-08-15 16:08:39 +0100

    Added Adaptive Boosting

--- visualisations/algorithms_comparison.py
+++ visualisations/algorithms_comparison.py
@@ -1,18 +1,16 @@
-from supervised.classification.decisionTreeClassifier import DecisionTreeClassifier
-from supervised.classification.naiveBayes import NaiveBayesClassifier
-from supervised.classification.randomForestClassifier import RandomForestClassifier
-from supervised.classification.gradientBoostingClassifier import GradientBoostingClassifier
-from supervised.classification.logisticRegression import LogisticRegression
-from supervised.classification.KNearestNeighbor import KNN
-from supervised.classification.adaBoost import AdaBoost
-
-from color_palette import two_colors, two_colors_map
-
+import matplotlib
+import matplotlib.pyplot as plt
+import numpy as np
 from sklearn.datasets import make_classification, make_moons, make_circles
 from sklearn.model_selection import train_test_split
-import matplotlib.pyplot as plt
-import matplotlib
-import numpy as np
+
+from color_palette import two_colors_map
+from supervised.classification.KNearestNeighbor import KNN
+from supervised.classification.adaBoost import AdaBoost
+from supervised.classification.decisionTreeClassifier import DecisionTreeClassifier
+from supervised.classification.logisticRegression import LogisticRegression
+from supervised.classification.naiveBayes import NaiveBayesClassifier
+from supervised.classification.randomForestClassifier import RandomForestClassifier
 
 matplotlib.use("TkAgg")
 
@@ -23,10 +21,10 @@ if __name__ == '__main__':
 
     # Create the dataset
     datasets = {
-        'linear_2': make_classification(n_features=2, n_redundant=0, n_informative=2),
-        'linear': make_classification(n_features=2, n_redundant=0, n_informative=2),
-        'moons': make_moons(noise=0.3, random_state=0),
-        'circles': make_circles(noise=0.2, factor=0.5, random_state=1)
+        'linear_2': make_classification(n_features=2, n_redundant=0, n_informative=2, random_state=42),
+        'linear': make_classification(n_features=2, n_redundant=0, n_informative=2, random_state=20),
+        'moons': make_moons(noise=0.3, random_state=42),
+        'circles': make_circles(noise=0.2, factor=0.5, random_state=20)
     }
 
     # Create train and test sets
@@ -43,13 +41,15 @@ if __name__ == '__main__':
     models = {
         'K-Nearest Neighbor': KNN(k=5),
         'Decision Tree': DecisionTreeClassifier(max_depth=5),
-        'Random Forest': RandomForestClassifier(num_estimators=5, max_depth=5, max_features=2),
+        'Random Forest': RandomForestClassifier(num_estimators=20, max_depth=6, max_features=2),
         'Logistic Regression': LogisticRegression(learning_rate=.01, iterations=5000),
-        'Naive Bayes': NaiveBayesClassifier()
+        'Naive Bayes': NaiveBayesClassifier(),
+        'Adaptive Boosting': AdaBoost(num_estimators=100)
     }
 
     # Start the pyplot grid
     figure, axes = plt.subplots(len(datasets), len(models) + 1, figsize=(27, 9))
+    figure.suptitle('Machine Learning - ELI5 - CLI5')
 
     # Plot the input data
     for i, data_name in enumerate(datasets):
