eead797479223b1887348cafc142022e35ec1c4f
Saad Chaouki <sd.chaouki@gmail.com>
2021-08-15 21:33:13 +0100

    Added function

--- statistics/tests.py
+++ statistics/tests.py
@@ -19,26 +19,30 @@ class StatisticalTest(object, metaclass=abc.ABCMeta):
 class ZTest(StatisticalTest):
 
     @staticmethod
-    def test(sample: np.array, population_mean: float, population_variance: float) -> float:
+    def test(sample: np.array, population_mean: float, population_std: float) -> float:
         """
         Function to compute the Z-score
         Formula:
             Z = (x - u) / (var / sqrt(n))
             where:
-
-
+                Z: Compute z score
+                x: Sample Average
+                u: Population Average
+                var: Population Standard Deviation
+                n: Sample Size
         """
-        z_score = (sample.mean() - population_mean) / (population_variance / np.sqrt(len(sample)))
+        z_score = (sample.mean() - population_mean) / (population_std / np.sqrt(len(sample)))
         return z_score
 
-    def __call__(self, sample: np.array, population_mean: float, population_variance: float):
+    def __call__(self, sample: np.array, population_mean: float, population_std: float):
         return self.test(sample=sample,
                          population_mean=population_mean,
-                         population_variance=population_variance)
+                         population_variance=population_std)
 
 
 if __name__ == '__main__':
     print('saad')
-    s = ZTest.test(np.array([0, 1, 0, 0, 0]), .2, 100)
-
-
+    x = np.array([0, 1, 0, 0, 0])
+    s = ZTest.test(x, .3, 100)
+    print(s)
+    print(x.mean())
