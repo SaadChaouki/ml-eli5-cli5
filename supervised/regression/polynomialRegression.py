625a69edadf4737e41c58193873cf8a54273d7f0
saad <sd.chaouki@gmail.com>
2021-09-04 13:52:45 +0100

    Data types

--- supervised/regression/polynomialRegression.py
+++ supervised/regression/polynomialRegression.py
@@ -1,16 +1,18 @@
 from processing.polynomialTransformer import PolynomialTransformation
 from supervised.base.baseRegression import BaseRegression
 
+import numpy as np
+
 
 class PolynomialRegression(BaseRegression):
-    def __init__(self, iterations=5000, learning_rate=.01, degree=2):
+    def __init__(self, iterations: int = 5000, learning_rate: float = .01, degree: int = 2):
         super(PolynomialRegression, self).__init__(iterations=iterations, learning_rate=learning_rate)
         self.poly_transformed = PolynomialTransformation(degree=degree, include_bias=False)
 
-    def fit(self, X, y):
+    def fit(self, X: np.array, y: np.array):
         X = self.poly_transformed.transform(X)
         return super(PolynomialRegression, self).fit(X, y)
 
-    def transform_predict(self, X):
+    def transform_predict(self, X: np.array):
         X = self.poly_transformed.transform(X)
         return super(PolynomialRegression, self).predict(X)
